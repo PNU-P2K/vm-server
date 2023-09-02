b35bede1a5d1fe52981c0ebc666b47d9d11c9508
suuding <tngus4789@naver.com>
2023-09-02 23:31:01 +0900

    feat : aes 암호화, 복호화 이용해 컨테이너, 이미지 id 암호화, 복호화

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 import os, time
+import base64
+import hashlib
+from Crypto.Cipher import AES
 
 app = Flask(__name__)
+
+baseImageId = '9e4131d0' # kasm window 이미지 
+
+BS = 16
+pad = (lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode())
+unpad = (lambda s: s[:-ord(s[len(s)-1:])])
+
 
 # 컨테이너 관련 명령어 
 def createContainerCmd(port, pwd, imageId) : # vm+port 이름의 컨테이너 생성 
@@ -29,7 +39,28 @@ def deleteImgCmd(imageId) :             # imageid로 이미지 삭제
 def pullImgCmd() :      # harbor에서 kasm 이미지 pull -> 이미 pull 받아짐
     return "docker pull registry.p2kcloud.com/base/vncdesktop"
 
-baseImageId = '9e4131d0' # kasm window 이미지 
+class AESCipher(object):
+    def __init__(self, key):
+        self.key = hashlib.sha256(key.encode()).digest()
+    
+    def encrypt(self, message):
+        message = message.encode()
+        raw = pad(message)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.__iv().encode('utf8'))
+        enc = cipher.encrypt(raw)
+        return base64.b64encode(enc).decode('utf-8')
+    
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.__iv().encode('utf8'))
+        dec = cipher.decrypt(enc)
+        return unpad(dec).decode('utf-8')
+    
+    def __iv(self):
+        return chr(0) * 16
+
+key = "thisiskey"
+aes = AESCipher(key)
 
 
 # spring 서버에서 컨테이너 생성 요청이 왔을 때, base 이미지로 컨테이너 생성하고 이미지 저장 
@@ -43,14 +74,16 @@ def create():
     stream1 = os.popen(createContainerCmd(port, pwd, baseImageId))
     containerId = stream1.read()[:12]
     time.sleep(5)
+    enContainerId = aes.encrypt(containerId) # containerId 암호화 
     
     stream2 = os.popen(createImgCmd(containerId, userId, port))
     imageId = stream2.read()[7:20]
+    enImageId = aes.encrypt(imageId)  # imageId 암호화 
     
     response = {
             'port': port,
-            'containerId' : containerId,
-            'imageId' : imageId
+            'containerId' : enContainerId,
+            'imageId' : enImageId
         }
     
     return jsonify(response), 200
@@ -64,16 +97,19 @@ def load() :
     requestDTO = request.get_json()
     print("[load requestDTO] ", requestDTO)
     userId, port, pwd, imageId = str(requestDTO['id']), str(requestDTO['port']), str(requestDTO['password']), str(requestDTO['key'])
+    deImageId = aes.decrypt(imageId)
     
-    stream1 = os.popen(createContainerCmd(port, pwd, imageId))
-    containerId = stream1.read()[:12]
+    stream1 = os.popen(createContainerCmd(port, pwd, deImageId))
+    newContainerId = stream1.read()[:12]
+    enContainerId = aes.encrypt(newContainerId)
     
-    stream2 = os.popen(createImgCmd(containerId, userId, port))
-    imageId = stream2.read()[7:20]
+    stream2 = os.popen(createImgCmd(newContainerId, userId, port))
+    newImageId = stream2.read()[7:20]
+    enImageId = aes.encrypt(newImageId)
     
     response = {
-        'containerId' : containerId,
-        'imageId' : imageId
+        'containerId' : enContainerId,
+        'imageId' : enImageId
     }
     
     return jsonify(response), 200
@@ -87,8 +123,9 @@ def start():
     requestDTO = request.get_json()
     print("[start requestDTO] ", requestDTO)
     port, containerId = str(requestDTO['port']), str(requestDTO['containerId'])
+    deContainerId = aes.decrypt(containerId)
     
-    os.popen(startContainerCmd(containerId))
+    os.popen(startContainerCmd(deContainerId))
     
     response = {
             'port' : port,
@@ -106,8 +143,9 @@ def stop():
     requestDTO = request.get_json()
     print("[stop requestDTO] ", requestDTO)
     port, containerId = str(requestDTO['port']), str(requestDTO['containerId'])
+    deContainerId = aes.decrypt(containerId)
     
-    os.popen(stopContainerCmd(containerId))
+    os.popen(stopContainerCmd(deContainerId))
     
     response = {
             'port' : port,
@@ -126,12 +164,14 @@ def save() :
     print("[save requestDTO] ", requestDTO)
     userId, port, pwd = str(requestDTO['id']), str(requestDTO['port']), str(requestDTO['pwd'])
     containerId, imageId = str(requestDTO['containerId']), str(requestDTO['imageId'])
+    deContainerId, deImageId = aes.decrypt(containerId), aes.decrypt(imageId)
     
-    stream1 = os.popen(createImgCmd(containerId, userId, port))
+    stream1 = os.popen(createImgCmd(deContainerId, userId, port))
     newImageId = stream1.read()[7:20]
+    enImageId = aes.encrypt(newImageId)
     print("1 : ", stream1.read())
     
-    stream2 = os.popen(deleteImgCmd(imageId))
+    stream2 = os.popen(deleteImgCmd(deImageId))
     print("2 : ", stream2.read())
     
     stream3 = os.popen(pushImgCmd(userId, port))
@@ -139,10 +179,11 @@ def save() :
     time.sleep(3)
     
     print("newImageId : ", newImageId)
+    print("ennewImageId : ", enImageId)
     
     response = {
             'containerId' : containerId,
-            'imageId' : newImageId
+            'imageId' : enImageId
         }
     
     return jsonify(response), 200
@@ -157,9 +198,10 @@ def delete():
     print("[delete requestDTO] ", requestDTO)
     userId, port = str(requestDTO['id']), str(requestDTO['port'])
     containerId, imageId = str(requestDTO['containerId']), str(requestDTO['imageId'])
+    deContainerId, deImageId = aes.decrypt(containerId), aes.decrypt(imageId)
     
-    os.popen(deleteContainerCmd(containerId))
-    os.popen(deleteImgCmd(imageId))
+    os.popen(deleteContainerCmd(deContainerId))
+    os.popen(deleteImgCmd(deImageId))
     
     response = {
             'port' : port,
