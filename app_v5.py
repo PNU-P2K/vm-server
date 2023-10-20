ea48bb2686f6ffaadc8dd6dbe27fce36850966fb
sososo0 <sh0000@pusan.ac.kr>
2023-10-20 11:30:36 +0900

    fix: Modify kubectl command in update yaml func

--- app_v5.py
+++ app_v5.py
@@ -171,7 +171,7 @@ def updateDeploymentYaml(deploymentName, namespace, deploymentYaml):
     print("d: "+deploymentName)
     print("n: "+ namespace)
     print("path: "+deploymentYaml)
-    return "kubectl get deployment "+deploymentName+" -n "+namespace+" --kubeconfig /root/kubeconfig.yml -o yaml > "+deploymentYaml
+    return "kubectl get deployment "+deploymentName+" -n default -o yaml > "+deploymentYaml+" --kubeconfig /root/kubeconfig.yml"
 
 # yaml 파일 업데이트 함수 - service 
 def updateServiceYaml(serviceName, namespace, serviceYaml):
